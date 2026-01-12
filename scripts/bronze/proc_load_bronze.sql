09206b4ac0a74d1fa1203daadb1405417cecde74
Nurbek009 <49669239+Nurbek009@users.noreply.github.com>
2026-01-12 23:04:29 +0500

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,6 +1,9 @@
-EXEC bronze.load_bronze
-
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
+/*
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+    EXEC bronze.load_bronze;
+*/CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
 	BEGIN TRY
